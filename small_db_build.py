15cb57cbfcbee5ff12a037d7974151167b4de7e5
ElaineBao <elaine.bao@foxmail.com>
2017-04-20 14:12:23 +0800

    fix small db build bug

--- small_db_build.py
+++ small_db_build.py
@@ -22,19 +22,27 @@ def classify_images(trainlist):
     classify images based on their groundtruth object labels
     """
     class_dict = {}
+    print len(trainlist)
+    obj_num = 0 
     for image_index in trainlist:
         filename = os.path.join(_DET_path, 'Annotations/DET/train', image_index + '.xml')
         tree = ET.parse(filename)
         objs = tree.findall('object')
         num_objs = len(objs)
         assert num_objs != 0, "num of objs = 0 in {}".format(filename)
+        old_cls = ''
         for obj in objs:
             cls = obj.find('name').text.lower().strip()
+            if cls == old_cls:
+                continue
             if cls in class_dict:
                 class_dict[cls].append(image_index)
             else:
                 class_dict[cls]=[image_index]
-            print "image_index:{}, class:{}".format(image_index,cls)
+            old_cls = cls
+            obj_num = obj_num + 1
+            # print "image_index:{}, class:{}".format(image_index,cls)
+    print "obj_num:{}".format(obj_num)
 
     return class_dict
 
@@ -44,28 +52,40 @@ def split_db(class_dict, num_of_splits = 10):
     for cls_name in class_dict.keys():
         cls_index = class_dict[cls_name]
         cls_index_num =len(cls_index)
+        print "cls:{},num of images:{}".format(cls_name, cls_index_num)
         for split_index in xrange(num_of_splits):
             # for each split, images of every class is averaged
             cls_index_begin = split_index * cls_index_num / num_of_splits
             cls_index_end = (split_index + 1) * cls_index_num / num_of_splits
-            print "cls_name:{}, split:{}, index:{}-{}".format(cls_name,split_index, cls_index_begin, cls_index_end)
-            if cls_index_end >= cls_index_num:
-                cls_index_end = cls_index_num - 1
+            #  print "cls_name:{}, split:{}, index:{}-{}".format(cls_name,split_index, cls_index_begin, cls_index_end)
+            if cls_index_end > cls_index_num:
+                cls_index_end = cls_index_num
             if split_index in split_image_index:
-                split_image_index[split_index].extend(cls_index[cls_index_begin:cls_index_end])
+                for img_path in cls_index[cls_index_begin:cls_index_end]:
+                    print img_path
+                    split_image_index[split_index].append(img_path)
+                # print split_image_index[split_index]
             else:
-                split_image_index[split_index] = [cls_index[cls_index_begin:cls_index_end]]
+                split_image_index[split_index] = [cls_index[cls_index_begin]]
+                for img_path in cls_index[cls_index_begin+1:cls_index_end]:
+                    print img_path
+                    split_image_index[split_index].append(img_path)
+
+            print "split_image_index[{}][0]:{},{}".format(split_index,type(split_image_index[split_index][0]),\
+                  len(split_image_index[split_index][0]))
 
     return split_image_index
 
 
 def save_train_txt(split_image_index, savepath):
-    for i, image_index_per_split in enumerate(split_image_index.values()):
+    for i, split_key in enumerate(split_image_index.keys()):
         split_i_savepath = os.path.join(savepath, 'train_small_'+str(i)+'.txt')
         print "save to {}".format(split_i_savepath)
-        with open(split_i_savepath, 'w') as f:
-            for image_index in image_index_per_split[0]:
-                print image_index
+        with open(split_i_savepath,'w') as f:
+            for image_index in split_image_index[split_key]:  
+                print "image_index_size in split:{}".format(len(image_index))
+                # print image_index
+                # print type(image_index)
                 f.write(image_index)
                 f.write('\n')
         f.close()
